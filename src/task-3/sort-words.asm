673befef931ad0bf6713f4b11d904ecc55e0bc9b
NuclearLion <dando.ds11@gmail.com>
2023-05-28 03:11:51 +0300

    get_words hopefuly working

--- src/task-3/sort-words.asm
+++ src/task-3/sort-words.asm
@@ -8,6 +8,7 @@ section .data
 	delimiters db ' ,.', 10, 0
 	endl db 10, 0
 	word_cnt dd 0
+	sep_zero db 0
 
 section .text
 	extern strtok
@@ -29,9 +30,14 @@ get_words:
     enter 0, 0
 	pusha
 
-	xor edi, edi
-	xor esi, esi
-	xor ecx, ecx
+	mov esi, [ebp + 12] ; words
+	mov ecx, [ebp + 16] ; number_of_words
+
+clear_array:
+	mov edi, [esi]
+	mov byte [edi], 0
+	add esi, 4
+	loop clear_array
 
 	mov edi, [ebp + 8] ; s
 	mov esi, [ebp + 12] ; words
@@ -46,71 +52,46 @@ get_words:
 	call strtok ; strtok(s, delimiters)
 	add esp, 8
 
-
 	; mov esi, eax ; words[0] = strtok(s, delimiters)
 	push eax
 	push dword [esi]
 	call strcat
 	add esp, 8
 
-	; PRINTF32 `before printf of pula\n\x0`
-	; PRINTF32 `value of esi: %s\x0\n`, esi
-	; PRINTF32 `after printf of pula\n\x0`
-
-	mov edx, endl
+	mov edx, endl ; edx = \n
 
 	push edx ; src
 	push dword [esi] ; dest
-	call strcat
+	call strcat ; strcat(words[0], \n)
 	add esp, 8
 
-	PRINTF32 `before printf of pula\n\x0`
-	PRINTF32 `%s\x0`, [esi]
-	PRINTF32 `after printf of pula\n\x0`
+process_tokens: 
+	dec dword [word_cnt] ; word_cnt--
+	cmp dword [word_cnt], 0 ; if word_cnt == 0
+	je end_process_tokens ; return
 
-process_tokens:
-	; cmp eax, 0 ; check if strtok returned NULL
-	dec dword [word_cnt]
-	cmp dword [word_cnt], 0
-	je end_process_tokens
-
-	mov edx, delimiters
-	push edx
-	push 0
+	mov edx, delimiters ; edx = delimiters
+	push edx ; delimiters
+	push 0 ; s = NULL
 	call strtok ; strtok(NULL, delimiters)
 	add esp, 8
 
-	; PRINTF32 `%s\x0`, eax
-	mov ebx, eax
+	mov ebx, eax ; save eax in ebx
 
-	; push eax
-	; push dword [esi]
-	; call strcat ; strcat(words[i], token)
-	; add esp, 8
-	push eax
-	push dword [esi]
-	call strcat
-	add esp, 8
+	push eax ; src
+	push dword [esi] ; dest
+	call strcat ; concatenate last word from string in the words array
+	add esp, 8 
 
-	; PRINTF32 `before printf of pula\n\x0`
-	; PRINTF32 `value of esi: %s\x0\n`, [esi]
-	; PRINTF32 `after printf of pula\n\x0`
-
-	mov edx, endl
+	mov edx, endl ; edx = \n
 
 	push edx ; src
 	push dword [esi] ; dest
-	call strcat
+	call strcat ; strcat(words[i], \n)
 	add esp, 8
-	PRINTF32 `before printf of pula\n\x0`
-	PRINTF32 `value of esi: %s\x0\n`, [esi]
-	PRINTF32 `after printf of pula\n\x0`
 
-	; PRINTF32 `edi value:%s\n\x0`, edi
-
-	mov eax, ebx
-	jmp process_tokens
-
+	mov eax, ebx ; restore eax
+	jmp process_tokens ; repeat
 
 end_process_tokens:
 
